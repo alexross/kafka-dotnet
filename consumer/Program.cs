a1c0a13d8e43eead290b1fca34154d76167fe75b
Alex Ross <imalexross@gmail.com>
2017-12-11 23:14:46 -0800

    (add) initial commit

---
+++ consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public async Task Consume()
+        {
+            var config = new Dictionary<string, object>();
+
+            using (var consumer = new Consumer<Null, string>(config, null, null)) 
+            {
+                consumer.OnLog += (sender, e) => Console.WriteLine($"Info: {e.Message}");
+                consumer.OnError += (sender, e) => Console.WriteLine($"Error: {e.Reason}");
+                consumer.OnMessage += (sender, e) => {
+                    // Message received
+                    Console.WriteLine($"{e.Topic}@{e.Timestamp.UtcDateTime}: {e.Value}");
+                };
+
+            }
+        }
+    }
+}
