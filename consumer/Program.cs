937bec0cae18421057f44f94e94841786eb960d8
Alex Ross <imalexross@gmail.com>
2017-12-13 22:41:38 -0800

    (add) fleshed out the consumer a bit, but not sure if confluent lib will play nicely with .net core

--- consumer/Program.cs
+++ consumer/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
 
 namespace consumer
 {
@@ -9,22 +13,31 @@ namespace consumer
     {
         static void Main(string[] args)
         {
+            // todo figure out why command line args aren't working for vs mac
+            string brokerList = "123"; // args[0];
+            var topics = new List<string>() { "abc", "def" }; // args.Skip(1).ToList();
 
-        }
-
-        public async Task Consume()
-        {
-            var config = new Dictionary<string, object>();
-
-            using (var consumer = new Consumer<Null, string>(config, null, null)) 
+            var config = new Dictionary<string, object>()
             {
-                consumer.OnLog += (sender, e) => Console.WriteLine($"Info: {e.Message}");
-                consumer.OnError += (sender, e) => Console.WriteLine($"Error: {e.Reason}");
-                consumer.OnMessage += (sender, e) => {
-                    // Message received
-                    Console.WriteLine($"{e.Topic}@{e.Timestamp.UtcDateTime}: {e.Value}");
-                };
+                { "group-id", "consumer-dotnet" },
+                { "bootstrap-servers", brokerList }
+            };
 
+            // todo can't load librdkafka.redist
+            using (var consumer = new Consumer<Ignore, string>(config, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                consumer.Assign(new List<TopicPartitionOffset>() { new TopicPartitionOffset(topics.First(), 0, 0) });
+
+                consumer.OnLog += (_, e) => Console.WriteLine($"Info: {e.Message}");
+                consumer.OnError += (_, e) => Console.WriteLine($"Critical Error: {e.Reason}");
+                consumer.OnConsumeError += (_, e) => Console.WriteLine($"Consume Error: {e.Value}");
+                consumer.OnMessage += (_, e) => Console.WriteLine($"{e.Topic}@{e.Timestamp.UtcDateTime}: {e.Value}");
+
+                while (!Console.KeyAvailable) 
+                {
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("\nShutting down...");
             }
         }
     }
